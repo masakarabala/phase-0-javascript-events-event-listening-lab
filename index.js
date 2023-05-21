65f9eb1f2240dbf74455ea4603495ce88047fcc4
masakarabala <48476935+masakarabala@users.noreply.github.com>
2023-05-21 22:43:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-function addingEventListener() {
+
+function addingEventListener(){
+
+    const button = document.getElementById('button');
+    button.addEventListener('click', function() {
+        alert('I was clicked!')
+    });
 }
+//or
+/*const button = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+button.addEventListener('click', clickAlert);
+*/
+
+
+
+
